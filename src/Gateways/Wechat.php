c93fc99b6cd7324774c96141258f6866fcdbc6d5
xiangzhangdd01 <38678791+xiangzhangdd01@users.noreply.github.com>
2018-10-29 15:46:04 +0800

    Update Wechat.php

--- src/Gateways/Wechat.php
+++ src/Gateways/Wechat.php
@@ -315,4 +315,33 @@ class Wechat implements GatewayApplicationInterface
 
         throw new InvalidGatewayException("Pay Gateway [{$gateway}] Must Be An Instance Of GatewayInterface");
     }
+    
+     /**
+     * 撤销订单支付
+     * 交易返回失败或支付系统超时，调用该接口撤销交易。如果此订单用户支付失败，微信支付系统会将此订单关闭；如果用户支付成功，微信支付系统会将此订单资金退还给用户。
+
+    注意：7天以内的交易单可调用撤销，其他正常支付的单如需实现相同功能请调用申请退款API。提交支付交易后调用【查询订单API】，没有明确的支付结果再调用【撤销订单API】。
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @param array $order
+     *
+     * @throws GatewayException
+     * @throws InvalidSignException
+     * @throws \Yansongda\Pay\Exceptions\InvalidArgumentException
+     *
+     * @return Collection
+     */
+    public function reverse($order): Collection
+    {
+        $this->payload = Support::filterPayload($this->payload, $order, true);
+
+        Log::info('Starting To reverse An Wechat Order', [$this->gateway, $this->payload]);
+
+        return Support::requestApi(
+            'secapi/pay/reverse',
+            $this->payload,
+            true
+        );
+    }
 }
